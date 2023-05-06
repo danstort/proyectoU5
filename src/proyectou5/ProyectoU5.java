636af317dc4e7325b3aa65e06b8e9ec09b81f31b
danie <danie@DanielsLAPTOP>
2023-05-07 00:00:34 +0200

    Segunda versión

--- src/proyectou5/ProyectoU5.java
+++ src/proyectou5/ProyectoU5.java
@@ -14,7 +14,34 @@ public class ProyectoU5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CCuenta Cuenta1;
+        double saldoActual;
+        
+        operativa_cuenta(0);
     }
+
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta Cuenta1;
+        double saldoActual;
+        Cuenta1 = new CCuenta("Manuel Mantecas","0000-1234-55-123456789",3000,0);
+        try 
+        {
+            Cuenta1.retirar(2300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al retirar");
+        }
+        try
+        {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al ingresar");
+        }
+        saldoActual = Cuenta1.getEstado();
+        System.out.println("El saldo actual es"+ saldoActual );
+    }
+
     
 }
